b3e975ced235e59af366d93f4783f90b5862948e
elkorn <helluinster@gmail.com>
2013-05-02 13:19:50 +0200

    removing leftover files

--- kernel_gaussian.m
+++
@@ -1,6 +0,0 @@
-function output_arg = kernel_gaussian(u)
-%KERNEL_GAUSSIAN The gaussian kernel.
-    e = exp(1);
-    output_arg = (sqrt(2*pi)^-1)*(e^(-0.5*u^2));
-end
-
--- kernel_triangle.m
+++
@@ -1,5 +0,0 @@
-function f = kernel_triangle(z)
-%TRIANGLE The triangular kernel.
-a = sqrt(6);
-z = abs(z);
-f = (z<=a) .* (1 - z/a) / a;

cd4066ae8ce0eca0146b7f08c099d6223d009962
elkorn <helluinster@gmail.com>
2013-05-02 13:01:59 +0200

    KDE working. Fine-tuning and optimization are still in order.

--- kde.m
+++ kde.m
@@ -1,17 +1,17 @@
-function y = kde(x, kernelfn)
+function y = kde(x, t, kernelfn)
 %KDE kernel density estimation of a given function.
 %   x - input function data
 %   kernelfn - the kernel estimation function to be used
 
     n = length(x);
-    y = zeros(1,n);
-    h = nrd(x);
-    for i = 1 : n
+    nT = length(t);
+    y = zeros(1,nT);
+    h = nrd0(x);
+    for i = 1 : nT
         for j = 1 : n
-            y(i) = y(i) + kernelfn((x(i) - x(j))/h);
+            y(i) = y(i) + kernelfn((t(i) - x(j))/h);
         end
     end
-    
-    y = y ./ (n*h);
+     y = y ./ (n*h);
 end
 
